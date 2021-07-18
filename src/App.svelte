a7e5321ba1bb485fe76f08c62f9691cde9dafcac
juancastellano115 <juancastellano115@gmail.com>
2021-07-18 19:37:11 +0200

    First version

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Strategy from "./components/Strategy.svelte";
+</script>
+
+<main>
+  <Strategy />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 0px;
+    margin: 0 0;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: 100%;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/components/Strategy.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { strategies } from "../oblique";
+  import Button, { Label, Icon } from "@smui/button";
+  import sample from "lodash.sample";
+  let phrase = "";
+
+  function getStrategy() {
+    phrase = sample(strategies);
+  }
+
+  getStrategy();
+</script>
+
+<div class="container">
+  <h1>{phrase.strategy}</h1>
+
+  <Button variant="raised" color="primary" on:click={getStrategy}>
+    <Label>Another</Label>
+    <Icon class="material-icons">replay</Icon>
+  </Button>
+</div>
+
+<style>
+  .container {
+    padding: 1em;
+  }
+  h1 {
+    color: white;
+    font-size: 4em;
+  }
+  @media (max-width: 640px) {
+    h1 {
+      font-size: 2.4em;
+    }
+  }
+</style>
